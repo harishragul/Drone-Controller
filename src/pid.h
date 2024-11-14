e6bb6b28414102e854a5c1c113288b06c8f979cf
harishragul <harishragul@protonmail.com>
2024-11-14 17:16:35 +0000

    update on pid calculation code

--- src/pid.h
+++ src/pid.h
@@ -10,6 +10,28 @@ struct PID
 
 float kp = 0, kv = 0, ki = 0;
 
-PID calculate_pid(float pitch, float roll, int desire_pitch, int desire_roll, int throttle){
-    
+float error;
+float derivative;
+float integral;
+float previousError;
+
+long previousTime = 0;
+float dt;
+
+int calculate_pid(float angle, int desire_angle){
+  unsigned long currentTime = millis();
+  dt = (currentTime - previousTime) / 1000.0;
+  previousTime = currentTime;
+
+  error = desire_angle - angle;
+  integral += error * dt;
+  derivative = (error - previousError) / dt;
+  float output = (kp * error) + (ki * integral) + (kv * derivative);
+  return output;  
 }
+
+
+void get_pwm(float pitch, float roll, float desire_pitch, float desire_roll, float throttle){
+  float pitch_pid = calculate_pid(pitch, desire_pitch);
+  float roll_pid = calculate_pid(roll, desire_roll);
+};
