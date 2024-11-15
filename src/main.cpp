854539afeb28db9736387bdb067586615e0ed459
harishragul <harishragul@protonmail.com>
2024-11-15 11:10:44 +0530

    update on pid and motor pwm code

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "rc.h"
 #include "esc.h"
 #include "mapping.h"
+#include "pid.h"
 
 void setup(){
   Serial.begin(9600);
@@ -14,12 +15,22 @@ void setup(){
 void loop(){
   IMU angle = get_angle();
   RC channel = get_channel();
+  PID pid = get_pwm(angle.pitch, angle.roll, -channel_to_angle(channel.ch2), -channel_to_angle(channel.ch1), channel.ch3);
 
 
   Serial.print(angle.pitch);
   Serial.print("\t");
   Serial.print(angle.roll);
   Serial.print("\t");
+  Serial.print(pid.pwm1);
+  Serial.print("\t");
+  Serial.print(pid.pwm2);
+  Serial.print("\t");
+  Serial.print(pid.pwm3);
+  Serial.print("\t");
+  Serial.print(pid.pwm4);
+  Serial.print("\t");
+  
   Serial.print(channel_to_angle(channel.ch1));
   Serial.print("\t");
   Serial.print(channel_to_angle(channel.ch2));
@@ -28,5 +39,5 @@ void loop(){
   Serial.print("\t");
   Serial.println(channel_to_angle(channel.ch4));
 
-  motor_pwm(channel.ch3, channel.ch3, channel.ch3, channel.ch3);
+  //motor_pwm(pid.pwm1, pid.pwm2, pid.pwm3, pid.pwm4);
 }
--- src/pid.h
+++ src/pid.h
@@ -8,30 +8,74 @@ struct PID
   int pwm4;
 };
 
-float kp = 0, kv = 0, ki = 0;
+#define kp 1
+#define kv 5
+#define ki 0.01
 
-float error;
-float derivative;
-float integral;
-float previousError;
+float pitch_error;
+float pitch_derivative;
+float pitch_integral;
+float pitch_previousError;
 
-long previousTime = 0;
-float dt;
+long pitch_previousTime = 0;
+float pitch_dt;
 
-int calculate_pid(float angle, int desire_angle){
-  unsigned long currentTime = millis();
-  dt = (currentTime - previousTime) / 1000.0;
-  previousTime = currentTime;
+int calculate_pitch_pid(float angle, int desire_angle){
+  unsigned long pitch_currentTime = millis();
+  pitch_dt = (pitch_currentTime - pitch_previousTime) / 1000.0;
+  pitch_previousTime = pitch_currentTime;
 
-  error = desire_angle - angle;
-  integral += error * dt;
-  derivative = (error - previousError) / dt;
-  float output = (kp * error) + (ki * integral) + (kv * derivative);
+  pitch_error = desire_angle - angle;
+  pitch_integral += pitch_error * pitch_dt;
+  pitch_derivative = (pitch_error - pitch_previousError) / pitch_dt;
+  float output = (kp * pitch_error) + (ki * pitch_integral) + (kv * pitch_derivative);
   return output;  
 }
 
+float roll_error;
+float roll_derivative;
+float roll_integral;
+float roll_previousError;
 
-void get_pwm(float pitch, float roll, float desire_pitch, float desire_roll, float throttle){
-  float pitch_pid = calculate_pid(pitch, desire_pitch);
-  float roll_pid = calculate_pid(roll, desire_roll);
+long roll_previousTime = 0;
+float roll_dt;
+
+int calculate_roll_pid(float angle, int desire_angle){
+  unsigned long roll_currentTime = millis();
+  roll_dt = (roll_currentTime - roll_previousTime) / 1000.0;
+  roll_previousTime = roll_currentTime;
+
+  roll_error = desire_angle - angle;
+  roll_integral += roll_error * roll_dt;
+  roll_derivative = (roll_error - roll_previousError) / roll_dt;
+  float output = (kp * roll_error) + (ki * roll_integral) + (kv * roll_derivative);
+  return output;  
+}
+
+int limit_pid(int pid){
+  (pid > 400) ? pid = 400:
+  (pid < -400) ? pid = -400:
+  pid = pid;
+
+  return pid;
+}
+
+int limit_pwm(int pwm){
+  (pwm > 1900) ? pwm = 1900:
+  (pwm < 1000) ? pwm = 1000:
+  pwm = pwm;
+
+  return pwm;
+}
+
+PID get_pwm(float pitch, float roll, float desire_pitch, float desire_roll, float throttle){
+  int pitch_pid = limit_pid(calculate_pitch_pid(pitch, desire_pitch));
+  int roll_pid = limit_pid(calculate_roll_pid(roll, desire_roll));
+
+  int pwm1 = limit_pwm(throttle - pitch_pid - roll_pid);
+  int pwm2 = limit_pwm(throttle - pitch_pid + roll_pid);
+  int pwm3 = limit_pwm(throttle + pitch_pid + roll_pid);
+  int pwm4 = limit_pwm(throttle + pitch_pid - roll_pid);
+
+  return {pwm1, pwm2, pwm3, pwm4};
 };
